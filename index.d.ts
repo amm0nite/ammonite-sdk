7773ce00f4ca765292f73f1e9290d3f2e8081c34
Ammonite <ammonite.myfox@gmail.com>
2018-01-13 22:26:04 +0100

    Fix type definitions

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 
 export class Cloud {
     constructor(token?: string);
-    fetchJobs(options, next?:(err: Error|null, jobs:any[]?) => void);
+    fetchJobs(options, next?:(err: Error|null, jobs?:any[]) => void);
     fetchJob(id: number, next?:(err:Error|null, job?) => void);
     createJob(job, next?:(err:Error|null, job?) => void);
 }
@@ -9,15 +9,15 @@ export class Cloud {
 export class MissionControl {
     constructor(token?: string);
     append(key:string, data, next?:(err:Error|null, event?) => void);
-    history(key:string, next?:(err:Error|null, events:any[]?) => void);
+    history(key:string, next?:(err:Error|null, events?:any[]) => void);
 }
 
 export class Backend {
     constructor(token?: string);
-    getDevices(next?:(err:Error|null, devices:any[]?) => void);
+    getDevices(next?:(err:Error|null, devices?:any[]) => void);
     claimDevice(secret:string, next?:(err:Error|null, device?) => void);
-    getRoutines(next?:(err:Error|null, routines:any[]?) => void);
-    getDeviceRoutines(uid:string, next?:(err:Error|null, routines:any[]?) => void);
+    getRoutines(next?:(err:Error|null, routines?:any[]) => void);
+    getDeviceRoutines(uid:string, next?:(err:Error|null, routines?:any[]) => void);
     createRoutine(data, next?:(err:Error|null, routine?) => void);
     bindRoutine(uid:string, name:string, next?:(err:Error|null) => void);
     unbindRoutine(uid:string, name:string, next?:(err:Error|null) => void);
